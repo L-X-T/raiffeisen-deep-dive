e79ffc67cc653a9b9dc8726d7d7e44d89ef4d72e
Alex Thalhammer <alex@angular-software.at>
2022-06-29 16:21:08 +0200

    08: 1. Unsubscribe with Subscription

--- nx-workspace/apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ nx-workspace/apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -1,18 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'airport',
   templateUrl: './airport.component.html'
 })
-export class AirportComponent implements OnInit {
-  airports: string[] = [];
+export class AirportComponent implements OnInit, OnDestroy {
+  airports$: Observable<string[]>;
+  airportsObserver: Observer<string[]>;
 
-  constructor(private airportService: AirportService) {}
+  // Unsubscribe with Subscription
+  airports: string[] = [];
+  airportsSubscription: Subscription | undefined;
+
+  // Unsubscribe with takeUntil Subject
+
+  // flags
+  isLoading = true;
+  isError = false;
+  errorMessage = '';
+
+  constructor(private airportService: AirportService, private router: Router) {}
 
   ngOnInit(): void {
-    this.airportService.findAll().subscribe((airports) => {
-      this.airports = airports;
+    this.airports$ = this.airportService.findAll().pipe(delay(3000));
+
+    this.airportsObserver = {
+      next: (airports) => {
+        this.airports = airports;
+        this.isLoading = false;
+        this.isError = false;
+      },
+      error: (err: HttpErrorResponse) => {
+        console.error(err.message);
+        this.isLoading = false;
+        this.isError = true;
+        this.errorMessage = err.message;
+      },
+      complete: () => console.log('complete')
+    };
+
+    this.airportsSubscription = this.airports$.subscribe(this.airportsObserver);
+
+    console.log('sync code');
+  }
+
+  ngOnDestroy(): void {
+    /*if (this.airportsSubscription) {
+      this.airportsSubscription.unsubscribe();
+    }*/
+
+    this.airportsSubscription?.unsubscribe();
+  }
+
+  navigate(): void {
+    this.router.navigate(['/home']).then((isSuccess) => {
+      this.isError = !isSuccess;
     });
   }
 }
