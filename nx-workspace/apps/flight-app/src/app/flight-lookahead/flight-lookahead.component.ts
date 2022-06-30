3b1ba8866563263759dd7261773dbd48a2d25c83
Alex Thalhammer <alex@angular-software.at>
2022-06-30 09:52:12 +0200

    09: Combine Streams

--- nx-workspace/apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
+++ nx-workspace/apps/flight-app/src/app/flight-lookahead/flight-lookahead.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Flight, FlightService } from '@flight-workspace/flight-lib';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { combineLatest, interval, Observable } from 'rxjs';
 import { debounceTime, distinctUntilChanged, filter, map, pairwise, startWith, switchMap, tap } from 'rxjs/operators';
 
 @Component({
@@ -16,16 +16,38 @@ export class FlightLookaheadComponent implements OnInit {
   isLoading = false;
 
   diff$: Observable<number>;
+  online = false;
 
   constructor(private flightService: FlightService) {}
 
   ngOnInit(): void {
-    this.flights$ = this.control.valueChanges.pipe(
+    /*this.flights$ = this.control.valueChanges.pipe(
       debounceTime(300),
       filter((input) => input.length > 2),
       distinctUntilChanged(),
       tap((input) => (this.isLoading = true)),
       switchMap((input) => this.load(input)),
+      tap((_) => (this.isLoading = false))
+    );*/
+
+    const input$: Observable<string> = this.control.valueChanges.pipe(
+      debounceTime(300),
+      filter((input) => input.length > 2),
+      distinctUntilChanged()
+    );
+
+    const online$: Observable<boolean> = interval(2000).pipe(
+      startWith(0),
+      map((_) => Math.random() < 0.5),
+      distinctUntilChanged(),
+      tap((online) => (this.online = online))
+    );
+
+    this.flights$ = combineLatest([input$, online$]).pipe(
+      filter(([_, online]) => online),
+      map(([input, _]) => input),
+      tap((input) => (this.isLoading = true)),
+      switchMap((input: string) => this.load(input)),
       tap((_) => (this.isLoading = false))
     );
 
